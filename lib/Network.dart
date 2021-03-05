e6b9a59937466c6d70f45a6af3b9483ac5edd5e2
cxdxng <marlon.scheer@t-online.de>
2021-03-05 20:29:25 +0100

    Added changes

--- lib/Network.dart
+++ lib/Network.dart
@@ -1,8 +1,11 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
 class GetData {
   void fetchData() async {
-    var response = await http.get(Uri.https("https://interaktiv.morgenpost.de",
-        "/corona-virus-karte-infektionen-deutschland-weltweit/"));
+    var response = await http.get(Uri.https("https://api.corona-zahlen.org","/districts/05315"));
+    print(jsonDecode(response.body));
+
   }
 }
