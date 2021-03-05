ce679753cc78bf33cbf92d2d9404a57900dfe74b
Marlon <marlon.scheer@t-online.de>
2021-03-05 13:00:09 +0100

    Added changes

--- lib/Network.dart
+++ lib/Network.dart
@@ -1,3 +1,8 @@
-class GetData{
+import 'package:http/http.dart' as http;
 
+class GetData {
+  void fetchData() async {
+    var response = await http.get(Uri.https("https://interaktiv.morgenpost.de",
+        "/corona-virus-karte-infektionen-deutschland-weltweit/"));
+  }
 }
